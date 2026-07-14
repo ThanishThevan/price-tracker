0e7c1ed74a86d8fd6522e348d64316cf362072c5
106421717 <106421717@student.swin.edu.au>
2026-07-14 17:13:33 +0800

    web scraping steps

--- tracker.py
+++ tracker.py
@@ -3,12 +3,13 @@ import requests
 
 url = "https://books.toscrape.com/"
 response = requests.get(url)
+response.encoding = "utf-8"
 
 soup = BeautifulSoup(response.text, "html.parser")
 
 books = soup.find_all("article", class_ ="product_pod")
 
-for books in books:
+for book in books:
     title = book.h3.a["title"]
     price = book.find("p", class_ ="price_color").text
     print(f"{title} - {price}")
